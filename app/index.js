4dc3ae2934c780a9d9ca115cd5b54cc31b5aec71
Zach Freedman <zfreedman92@gmail.com>
2018-07-02 23:52:01 -0400

    feat: Skeleton app
    Contains background image, centerpiece title
    Also configured webpack (using file/url loaders for fonts and images)

---
+++ app/index.js
@@ -0,0 +1,18 @@
+"use strict";
+import React from "react";
+import ReactDOM from "react-dom";
+require("./index.css");
+
+// import Centerpiece from "./components/centerpiece";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="centerpiece">
+        Zach
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+// use ES5 in this file
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]},
+      { test: /\.(png|jpg|gif)$/, use: [ "url-loader" ]},
+      // https://chriscourses.com/blog/loading-fonts-webpack
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+                outputPath: 'fonts/'
+            }
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+  mode: "development"
+};
