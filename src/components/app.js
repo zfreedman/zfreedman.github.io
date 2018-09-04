068c6a8adf7fff88e45f8cfdac5414ad97a81806
zfreedman <zfreedman92@gmail.com>
2018-09-03 20:14:51 -0700

    feat: Nuked src/

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        Zach's website is coming
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from "./components/app";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
